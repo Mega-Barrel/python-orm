2a038c784150d72866480768d6b442031253b601
Saurabh <saurabhjoshi7869@gmail.com>
2023-05-05 10:18:20 +0530

    modified code

--- main.py
+++ main.py
@@ -6,28 +6,17 @@ class Hero(SQLModel, table=True):
     name: str
     secret_name: str
     age: Optional[int] = None
-    
-hero_1 = Hero(id=1, name="Deadpond", secret_name="Dive Wilson")
-hero_2 = Hero(id=2, name="Spider-Boy", secret_name="Pedro Parqueador")
-hero_3 = Hero(id=3, name="Rusty-Man", secret_name="Tommy Sharp", age=48)
 
 # Create engine
-engine = create_engine("sqlite:///database.db")
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-# create table
-SQLModel.metadata.create_all(engine)
+# create engine
+engine = create_engine(sqlite_url)
 
-# Function to insert data
-def insert_data(data):
-    with Session(engine) as session:
-        session.add(data)
-        session.commit()
-
-def query_table(value):
-    with Session(engine) as session:
-        statement = select(Hero).where(Hero.name == value)
-        hero = session.exec(statement).first()
-        print(hero)
+def create_db_and_tables():
+    # creating table
+    SQLModel.metadata.create_all(engine)        
         
-if __name__ == "__main__":
-    query_table("Deadpond")
+if __name__ == "__main__":    
+    create_db_and_tables()

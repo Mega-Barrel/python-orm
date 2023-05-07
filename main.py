3e9d1a5aad5dcb730b5100defbd7e4a3f084962c
Saurabh <saurabhjoshi7869@gmail.com>
2023-05-07 14:53:30 +0530

    fixed imports, added create and fetch heroes function

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from typing import Optional
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import (
+    Field, 
+    Session, 
+    SQLModel, 
+    create_engine, 
+    select
+)
 
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -12,11 +18,36 @@ sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 # create engine
-engine = create_engine(sqlite_url)
+engine = create_engine(sqlite_url, echo=True)
 
 def create_db_and_tables():
     # creating table
-    SQLModel.metadata.create_all(engine)        
+    SQLModel.metadata.create_all(engine)
+    
+def create_heros():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    
+    with Session(engine) as session:    
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+        # Commit the changes
+        session.commit()
+        
+def select_heroes():
+    with Session(engine) as session:
+        statement = session.exec(select(Hero)).all()
+        print(statement)
+        # for result in results:
+        #     print(result)
+        #     print()
+    
+def main():
+    create_db_and_tables()
+    # create_heros()
+    select_heroes()
         
 if __name__ == "__main__":    
-    create_db_and_tables()
+    main()
